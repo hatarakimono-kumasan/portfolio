3e30ac37fe6ba71cba29e0d7094574edd7a02967
hatarakimonokumasan <hatarakimonokumasan@gmail.com>
2020-03-01 16:22:57 +1100

    トップへjs

---
+++ js/floating.js
@@ -0,0 +1,14 @@
+jQuery(window).on("scroll", function($) {
+    if (jQuery(this).scrollTop() > 100) {
+      jQuery('.floating').show();
+    } else {
+      jQuery('.floating').hide();
+    }
+  });
+  
+  jQuery('.floating').click(function () {
+    jQuery('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
